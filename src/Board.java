d0e4c56810d477708fc1d2514ab639cdfeb2d352
Harry Wong <harry10286@gmail.com>
2018-03-12 23:04:01 -0700

    Partial Stratego Code

--- src/Board.java
+++ src/Board.java
@@ -13,4 +13,18 @@ class Board{
 
 	int getWidth(){return width;}
 	int getHeight(){return height;}
+	
+	int[] getPosition(Piece p){
+		int result[] = new int[2];
+		for(int i = 0; i < width; ++i){
+			for(int j = 0; j < height; ++j){
+				if(pieces[i][j] == p){
+					result[0] = i;
+					result[1] = j;
+					return result;
+				}
+			}
+		}
+		return null;
+	}
 }
